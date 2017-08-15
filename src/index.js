bc0a5db325a9c31e19efd570494d10171529f358
keshen <hackersk@qq.com>
2017-08-15 13:30:24 +0800

    upd8

--- src/index.js
+++ src/index.js
@@ -30,6 +30,13 @@ function Bind(ref, $el, relation) {
 
     if (!Util.isNode($el)) return;
 
+    /**
+     * 绑定/渲染模式
+     * @type {String}
+     * @note "active"模式: 指定ref和$el之间的relation，主动进行双向绑定，不依赖于dom标签中的属性和模板；
+     *           绑定逻辑可分离为配置对象；可以达到渲染和绑定分离的目的，所以如果是SSR则更推荐这种模式.
+     *       "passive"模式: 依赖于dom标签内定义的属性和模板，进行解析；渲染与绑定逻辑耦合，模板即组件.
+     */
     var mode = Util.isObject(relation) ? 'active' : 'passive';
 
     if (mode === 'active') {
@@ -88,6 +95,7 @@ function Bind(ref, $el, relation) {
 var DefaultConf = {
     attrPrefix: 'm-',
     refSeparator: '/',
+    tmplGrammer: 'dollarbrace',
     tmplEngine: {
         parseDeps: function (tmpl) {
 
